1c6f6368fbd379f21d565212123df2aaabf15b11
Srividhya Sainath <83634591+Srividhya-Sainath@users.noreply.github.com>
2024-09-25 14:57:23 +0200

    Second Commit

--- helper.py
+++ helper.py
@@ -1,6 +1,32 @@
 import numpy as np
 import h5py
 import pandas as pd
+import logging
+import yaml
+
+def setup_logger(log_file_path):
+    logger = logging.getLogger('KernelProcessingLogger')
+    logger.setLevel(logging.INFO)
+    f_handler = logging.FileHandler(log_file_path)
+    f_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    f_handler.setFormatter(formatter)
+    logger.addHandler(f_handler)
+    
+    return logger
+
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+def file_contains_key(filepath, key):
+    """ Check if the h5 file contains the specified key """
+    if os.path.exists(filepath):
+        with h5py.File(filepath, 'r') as hf:
+            if key in hf.keys():
+                return True
+    return False
 
 def TOPk(similarity_matrix, k):
     n = similarity_matrix.shape[0]
